ba2b91002ae89cbb64cd4805391ed31870055c5e
Mike MacCana <mike.maccana@gmail.com>
2014-10-03 12:09:34 +0100

    Exit if the config file is bad

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var CSON = require('cson'),
 var loadConfig = function(configFileName) {
   configFileName = configFileName || DEFAULT_CONFIG_FILE;
   var config = CSON.parseFileSync(configFileName)
+  if ( Object.prototype.toString.call(config) === '[object Error]' ) {
+    log('Error loading config file', configFileName, ':', config)
+    process.exit(1)
+  }
   config.isProduction = ( process.env.NODE_ENV === 'production' );
   return config;
 }
